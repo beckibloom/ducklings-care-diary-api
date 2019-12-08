0c92381ab4290ab4bf484488dc3c016d37b759f1
Becki Blumer <rblumer13@gmail.com>
2019-12-08 17:50:05 -0600

    wrote seed scripts to seed some users, students, and diary entries into the database

---
+++ seeds/seed.ducklings_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, password, type)
+VALUES
+  ('teacher@email.com', 'password', 'teacher'),
+  ('parent1@email.com', 'parent1', 'parent'),
+  ('parent2@email.com', 'parent2', 'parent');
