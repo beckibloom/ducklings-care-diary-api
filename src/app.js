f0e6e1f5e865852c686c682755a8d113bce3cce5
Becki Blumer <rblumer13@gmail.com>
2019-12-08 18:06:19 -0600

    created auth-service, auth-router, and jwt-auth middleware; configured app and server to use knex and to access the auth-router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router');
 
 const app = express();
 
@@ -15,19 +16,22 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get('/api/*', (req, res) => {
-    res.json({ok: true});
-});
+app.use('/api/auth', authRouter);
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error}
-    }
-    res.status(500).json(response)
+const errorHandler = (error, req, res, next) => {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: "Server error" } }
+  } else {
+    response = { message: error.message, error }
+  };
+  res.status(500).json(response);
+}
+
+app.use(errorHandler);
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
 });
 
 module.exports = app;
--- src/config.js
+++ src/config.js
@@ -2,4 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://becki_pg:password@localhost/ducklings_care_diary',
+    JWT_SECRET: process.env.JWT_SECRET || 'BeAmbitiousLittleDucklings',
+    JWT_EXPIRY: '900000'
 }
