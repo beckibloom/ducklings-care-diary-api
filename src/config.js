2544beb13e12818571a479f482a4fc0c8906876d
Becki Blumer <rblumer13@gmail.com>
2019-12-10 04:23:39 -0600

    provisioning to deploy database

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://becki_pg:password@localhost/ducklings_care_diary',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://becki_pg:password@localhost/ducklings_test',
     JWT_SECRET: process.env.JWT_SECRET || 'BeAmbitiousLittleDucklings',
     JWT_EXPIRY: '900000'
 }
